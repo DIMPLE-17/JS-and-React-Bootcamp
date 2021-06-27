5227b0b1da11ffa69d613053058fb56fbe286746
DIMPLE-17 <83968485+DIMPLE-17@users.noreply.github.com>
2021-06-27 22:12:01 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info(){
+  return(
+    <div class="note">
+      <h1 >Javascript and React.js</h1>
+      <p >A Basic Web Development Bootcamp of JavaScript and ReactJS</p>
+      </div>
+  );
+}
+export default Info;
